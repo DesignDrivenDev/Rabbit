8cc49c0c55398810cf7ad4573ffa5bcb15c13f83
Padmalochan Barik <padmalochan.barik@astriadigital.com>
2025-05-21 18:15:41 +0530

    allowed origin in the backend

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,20 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = ['https://rabbit-qxmn.vercel.app', 'http://localhost:5173'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
+
+// app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
